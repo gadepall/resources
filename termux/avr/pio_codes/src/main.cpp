1df7e6fa769fa25d387c7cfc087687839899c0cf
gadepall <gadepall@gmail.com>
2019-07-07 22:23:26 +0530

    Add existing files

---
+++ termux/avr/pio_codes/src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+//LED_BUILTIN = 13;
+void setup() {
+pinMode(LED_BUILTIN,OUTPUT);
+
+}
+
+void loop() {
+digitalWrite(LED_BUILTIN,LOW);
+//digitalWrite(LED_BUILTIN,HIGH);
+}
